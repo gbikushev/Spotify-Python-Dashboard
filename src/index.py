8b4232f6e51ea16f3f2c6afd8e04b403c6f4225b
D00264223 <d00264223@student.dkit.ie>
2024-04-22 23:57:19 +0100

    DashTools Automatic Push - 04/22/2024 @ 23:57:19

--- src/index.py
+++ src/index.py
@@ -6,6 +6,7 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 from app import app
+from app import server
 # import all pages in the app
 from apps import US, IN, IE, home
 
